d1f5e197c6c6f7690de6433aa3fd4f2e8e70e02e
Matthias Kutscheid <eifelschaf@gmail.com>
2021-05-22 20:34:21 +0200

    erste Aufgaben geschafft

--- app/src/main/java/de/kleinelamas/klara/lernt/MainActivity.kt
+++ app/src/main/java/de/kleinelamas/klara/lernt/MainActivity.kt
@@ -3,17 +3,21 @@ package de.kleinelamas.klara.lernt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.kleinelamas.klara.lernt.code.Bedingungen
+import de.kleinelamas.klara.lernt.ui.theme.Green200
 import de.kleinelamas.klara.lernt.ui.theme.KlaraLerntTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,41 +25,76 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KlaraLerntTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(
-                        modifier = Modifier.padding(
-                            horizontal = 16.dp,
-                            vertical = 8.dp,
-                        )
-                    ) {
-                        Column {
-                            listOf(
-                                "roter Panda",
-                                "Braunbär",
-                                "großer Panda",
-                            ).map {
-                                Row {
-                                    Text(
-                                        it,
-                                        Modifier.padding(end = 4.dp)
-                                    )
-                                    Text(Bedingungen.istEinPanda(it))
-                                }
-                            }
-                            listOf(
-                                "Erdbeere",
-                                "Blaubär",
-                                "Blaubeere",
-                            ).map {
-                                Row {
-                                    Text(
-                                        it,
-                                        Modifier.padding(end = 4.dp)
-                                    )
-                                    Text(if (Bedingungen.istEinBaer(it)) "Füttern!" else "Futtern")
-                                }
-                            }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            backgroundColor = MaterialTheme.colors.primaryVariant,
+                            title = {
+                            Text("Bärenauslese")
+                        })
+                    },
+                    // A surface container using the 'background' color from the theme
+                    content = {
+                        PandaContent()
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PandaContent() {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    horizontal = 16.dp,
+                )
+                .verticalScroll(rememberScrollState())
+                .padding(vertical = 8.dp)
+        ) {
+            Column {
+                listOf(
+                    "roter Panda",
+                    "Braunbär",
+                    "großer Panda",
+                    "Eisbär",
+                    "Nasenbär",
+                    "Gummibär"
+                ).map {
+                    Card(
+                        Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth()) {
+                        Row(Modifier.padding(16.dp)) {
+                            Text(
+                                it,
+                                Modifier.padding(end = 4.dp)
+                            )
+                            Text(Bedingungen.istEinPanda(it))
+                        }
+                    }
+                }
+                listOf(
+                    "Erdbeere",
+                    "Blaubär",
+                    "Blaubeere",
+                    "Eisbär",
+                    "Teddybär",
+                    "Gummibär",
+                    "Fisch",
+                ).map {
+                    Card(
+                        Modifier
+                            .padding(4.dp)
+                            .fillMaxWidth(), backgroundColor = if (Bedingungen.istEinBaer(it)) MaterialTheme.colors.primaryVariant else MaterialTheme.colors.error) {
+                        Row(Modifier.padding(16.dp)) {
+                            Text(
+                                it,
+                                Modifier.padding(end = 4.dp)
+                            )
+                            Text(if (Bedingungen.istEinBaer(it)) "Füttern!" else "Futtern")
                         }
                     }
                 }
@@ -64,22 +103,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hallo $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KlaraLerntTheme {
-        Column(
-            modifier = Modifier.padding(
-                horizontal = 16.dp,
-                vertical = 8.dp,
-            )
-        ) {
-            Greeting("Panda")
-        }
+        PandaContent()
     }
 }
--- app/src/main/java/de/kleinelamas/klara/lernt/code/Bedingungen.kt
+++ app/src/main/java/de/kleinelamas/klara/lernt/code/Bedingungen.kt
@@ -4,11 +4,18 @@ class Bedingungen {
 
     companion object {
         fun istEinPanda(baer: String): String {
-
+            if (baer.contains("Panda")) {
+                return "ist ein Panda!"
+            }
             return "ist kein Panda"
         }
 
         fun istEinBaer(dingsi: String): Boolean {
+            if (dingsi == "Gummibär") {
+                return false
+            } else if (dingsi.contains("bär")) {
+                return true
+            }
             return false
         }
     }
